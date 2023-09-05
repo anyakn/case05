417119ab5e92626613ed542ae4df57bb56c67e12
Ulianashilk <u.shilkova@g.nsu.ru>
2023-09-05 10:22:29 +0700

    1

--- Uly.py
+++ Uly.py
@@ -1,1 +1,11 @@
-#help
+# fre - Flesch reading ease scale
+# ASL - average sentence length
+# ASW - average number of syllables per word
+# ASL = total words/total sentences
+# ASW = total syllables/total words
+tw =
+tsn =
+tsll =
+asl = tw/tsn
+asw = tsll/tw
+fre = 206.835 - 1.015 * ASL - 84.6 * ASW
